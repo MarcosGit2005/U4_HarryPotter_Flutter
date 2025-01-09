035ab550b256a4e5c8329556a86884ad2520421a
MarcosGit2005 <marcosrubiodiaz@gmail.com>
2025-01-09 18:12:01 +0100

    09012025 U4PA3

--- lib/data/repositories/character_repository_impl.dart
+++ lib/data/repositories/character_repository_impl.dart
@@ -13,7 +13,9 @@ class CharacterRepositoryImpl implements CharacterRepository {
       return Right(characterModels.map((model) =>
           Character(name: model.name, house: model.house,
               image: model.image.isNotEmpty?model.image:'https://ik.imagekit.io/hpapi/hagrid.png',
-              wand: 'Wand[${model.wand['wood']}, ${model.wand['core']}, ${model.wand['length']}]')
+              wand: 'Wand[${model.wand['wood']==null||model.wand['wood']==""?"No wood":model.wand['wood']}, '
+                  '${model.wand['core']==null||model.wand['core']==""?"No core":model.wand['core']}, '
+                  '${model.wand['length'] ?? 5}]')
       ).toList());
     } catch (e) {
       return Left(Exception('Error al cargar personajes'));
